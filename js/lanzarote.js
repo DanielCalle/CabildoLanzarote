f85199eb538b3ec92699a2db8f536960fc28d9a8
Daniel Calle <danielcallesanchez@gmail.com>
2018-02-07 16:59:24 +0100

    Imagenes redondas encima de carousel

--- js/lanzarote.js
+++ js/lanzarote.js
@@ -1,5 +1,4 @@
-$('#carouselExampleControls').carousel()
-@media screen and (max-height: 450px) {
-    .sidenav {padding-top: 15px;}
-    .sidenav a {font-size: 18px;}
-}
+$(document).ready(function(){
+    $('#carouselExampleControls').carousel()
+    $('[data-toggle="tooltip"]').tooltip();
+});
