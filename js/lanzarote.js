68d29534f85af5ce4e8c5fa2dc9e8d87ad824cfe
Daniel Calle <danielcallesanchez@gmail.com>
2018-02-07 23:18:53 +0100

    Arreglando conflicto en js

--- js/lanzarote.js
+++ js/lanzarote.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-$('#carouselExampleControls').carousel()
-=======
+
 $(document).ready(function(){
     $('#carouselExampleControls').carousel()
     $('[data-toggle="tooltip"]').tooltip();
 });
->>>>>>> 48b67d11d55fb00ff3800a3d4a032bd2c471f4f9
